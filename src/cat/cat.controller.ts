e7e4a87e5d3bf2ffd8d74f20c9e85c29d256c27d
VicenteFleitas <vicente1fleitas1@gmail.com>
2025-03-19 20:40:29 -0300

    feat: interfaces wip

--- src/cat/cat.controller.ts
+++ src/cat/cat.controller.ts
@@ -1,27 +1,52 @@
-import { Controller, Get, Query, Req, Post, HttpCode, Param } from '@nestjs/common';
+import { Controller, Get, Query, Req, Post, HttpCode, Param, Body } from '@nestjs/common';
 import { Request } from 'express';
+
+import { CreateCatDto } from "./create-cat.dto"
 
 @Controller("cats")
 export class CatsController {
 
+  // @Post()
+  // @HttpCode(204)
+  // create(): object {
+  //   return { message: 'This action adds a new cat' };
+  // }
+
   @Post()
-    //   @HttpCode(204)
-  create(): object {
-    return { message: 'This action adds a new cat' };
+  async create(@Body() createCatDto: CreateCatDto) {
+    let data = {
+      success: true,
+      data: createCatDto
+    }
+    return data;
   }
+
 
   @Get()
-  findAll(@Req() request: Request, @Query('uid') uid: number ): object {
-    console.log(JSON.stringify(request.body), uid)
-    return { message: "This action return all cats" };
+  async findAll(@Query('age') age: number, @Query('breed') breed: string) {
+    let data = {
+      success: true,
+      data: {
+        age: Number(age),
+        breed:breed
+      }
+    }
+    return data;
   }
 
 
-@Get(':id')
-findOne(@Param() params: any): string {
-  console.log(params.id);
-  return `This action returns a #${params.id} cat`;
-}
+  // @Get()
+  // findAll(@Req() request: Request, @Query('uid') uid: number ): object {
+  //   console.log(JSON.stringify(request.body), uid)
+  //   return { message: "This action return all cats" };
+  // }
+
+
+  @Get(':id')
+  findOne(@Param('id') id: string): string {
+    console.log(id);
+    return `This action returns a #${id} cat`;
+  }
 
 
 }
